b48a659199dd9378a12650f8a057823970d7e010
Joseph Grist <joe.grist@asdeqlabs.com>
2022-10-30 19:56:09 +1100

    Update API

--- app/shared/src/commonMain/kotlin/com/example/merchapp/API.kt
+++ app/shared/src/commonMain/kotlin/com/example/merchapp/API.kt
@@ -1,19 +1,34 @@
 import io.ktor.client.*
-import io.ktor.client.engine.*
+import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.coroutineScope
 
-class TodoApiClient constructor(
-    httpClientEngine: HttpClientEngine? = null) {
+class ApiClient {
+
     companion object {
-        const val BASE_ENDPOINT =
-            "http://jsonplaceholder.typicode.com"
+        const val BASE_ENDPOINT = "http://jsonplaceholder.typicode.com"
     }
 
-    private val client: HttpClient =
-        HttpClient(httpClientEngine!!) {
-            install(ContentNegotiation) {
-                json()
-            }
+    private val client: HttpClient = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
         }
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.HEADERS
+        }
+        expectSuccess = true
+    }
+
+    suspend fun makeRequest() = coroutineScope {
+        val response: HttpResponse = client.request("https://ktor.io/") {
+            // Configure request parameters exposed by HttpRequestBuilder
+        }
+        println(response.status)
+        client.close()
+    }
 }
