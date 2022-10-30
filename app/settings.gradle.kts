29f07f943e5df2414511531352d5e2d35660fde2
Joseph Grist <joe.grist@asdeqlabs.com>
2022-10-30 11:42:46 +1100

    Add KMP App

---
+++ app/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MerchApp"
+include(":androidApp")
+include(":shared")
---
+++ app/shared/src/commonMain/kotlin/com/example/merchapp/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.merchapp
+
+expect class Platform() {
+    val platform: String
+}
