c3beba1f402701a6ca7ba6d00ae23581732d49fa
Joe Grist <joseph.j.grist@gmail.com>
2022-11-04 08:47:25 +1100

    WIP

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -30,6 +30,7 @@ android {
 }
 
 dependencies {
+    val lifecycle_version = "2.2.0"
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
@@ -37,4 +38,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
 }
