19e5ea654c493a5b289684a507151092fcb88d1f
Joe Grist <joseph.j.grist@gmail.com>
2022-11-05 08:37:07 +1100

    WIP

--- app/androidApp/src/main/java/com/example/merchapp/android/MainActivity.kt
+++ app/androidApp/src/main/java/com/example/merchapp/android/MainActivity.kt
@@ -8,19 +8,30 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity()  {
 
+    fun getNavigationController(res: Int): NavController {
+        val navHostFragment = supportFragmentManager.findFragmentById(res) as NavHostFragment
+        return navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
             setContentView(R.layout.main)
             setSupportActionBar(findViewById(R.id.action_bar))
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-            val navController = navHostFragment.navController
+            val navController = getNavigationController(R.id.nav_host_fragment)
             NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = getNavigationController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
